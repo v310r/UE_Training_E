b4224a4c12d9250355e164e26530467bfbb5a912
v310r <webmail200601@gmail.com>
2022-07-11 14:51:44 +0300

    folders layout

--- HW_Calculator/Intermediate/Build/Win64/CalculatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ HW_Calculator/Intermediate/Build/Win64/CalculatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.32.31329
+// Compiler: 14.29.30145
 #include "D:/tech/studyingMaterials/UE/EPAM/UE_Training_E/HW_Calculator/Intermediate/Build/Win64/CalculatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ HW_Calculator/Intermediate/Build/Win64/CalculatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.32.31329
+#include "D:/tech/studyingMaterials/UE/EPAM/UE_Training_E/HW_Calculator/Intermediate/Build/Win64/CalculatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
